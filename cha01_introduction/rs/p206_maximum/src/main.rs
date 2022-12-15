598cb28c9864ca6371303a9668f041da6ef9b9e0
mulfranck <njifranclin@gmail.com>
2022-12-15 22:41:57 +0100

    Solved problem 1.2.6
    This pb was very challenging, rs conversion was not easy managing
    And type incompatible issues was a headache

--- cha01_introduction/rs/p206_maximum/src/main.rs
+++ cha01_introduction/rs/p206_maximum/src/main.rs
@@ -1,3 +1,40 @@
+use std::io::stdin;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Enter your Ten cards int separated by \"comma\"!");
+    // let list: String = String::from("12, 32, 24, 56,23");
+    let mut list: String = String::new();
+
+    stdin().read_line(&mut list)
+            .expect("Enter a list, boi!");
+
+    let str_numbers = list.split(',');
+    // println!("{:?}", str_numbers);
+    let str_numbers: Vec<&str> = str_numbers.collect();
+    let mut numbers: Vec<f64> = vec![];
+    
+    for numb in str_numbers {
+        let n  = numb.trim();
+        
+        let n = n.parse();
+        let n: f64 = n.unwrap();
+        numbers.push(n);
+    }
+
+    let n: usize = numbers.len();
+    
+    let mut j: i32 = 1;
+    let mut i: usize = 1;
+    let mut b = numbers[0];
+
+    while i < n  {
+        if numbers[i] > b {
+            b = numbers[i];
+            j = i.try_into().unwrap();
+        }
+        i += 1;
+    }
+    println!("You entered {n} Card Int which are {:?}", numbers);
+    println!("The largest card int is {b} at position {}.", j + 1);
+
 }
